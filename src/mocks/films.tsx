7f8ee4f935d391f09b0e0edebb245e20e8b47471
ZuevaDarya <zuevadasha55@gmail.com>
2023-11-01 14:57:06 +0300

    изменены моковые данные

--- src/mocks/films.tsx
+++ src/mocks/films.tsx
@@ -1,58 +1,69 @@
-import { FilmCardProps, MainPageProps } from '../types/types';
+import { FilmCardProps, PromoFilm } from '../types/types';
 
-export const FILM_INFO: FilmCardProps[] = [
+export const FILMS_INFO: FilmCardProps[] = [
   {
+    id: '1FBTCoG',
     filmTitle: 'Fantastic Beasts: The Crimes of Grindelwald',
     img: 'fantastic-beasts-the-crimes-of-grindelwald.jpg'
   },
   {
+    id: '2BR',
     filmTitle: 'Bohemian Rhapsody',
     img: 'bohemian-rhapsody.jpg'
   },
   {
+    id: '3M',
     filmTitle: 'Macbeth',
     img: 'macbeth.jpg'
   },
   {
+    id: '4A',
     filmTitle: 'Aviator',
     img: 'aviator.jpg'
   },
   {
+    id: '5WnttaK',
     filmTitle: 'We need to talk about Kevin',
     img: 'we-need-to-talk-about-kevin.jpg'
   },
   {
+    id: '6WWDitS',
     filmTitle: 'What We Do in the Shadows',
     img: 'what-we-do-in-the-shadows.jpg'
   },
   {
+    id: '7R',
     filmTitle: 'Revenant',
     img: 'revenant.jpg'
   },
   {
+    id: '8JE',
     filmTitle: 'Johnny English',
     img: 'johnny-english.jpg'
   },
   {
+    id: '9SI',
     filmTitle: 'Shutter Island',
     img: 'shutter-island.jpg'
   },
   {
+    id: '10PF',
     filmTitle: 'Pulp Fiction',
     img: 'pulp-fiction.jpg'
   },
   {
+    id: '11NCfOM',
     filmTitle: 'No Country for Old Men',
     img: 'no-country-for-old-men.jpg'
   },
   {
+    id: '12S',
     filmTitle: 'Snatc',
     img: 'snatch.jpg'
   },
 ];
 
-
-export const PROMO_FILM_DATA: MainPageProps = {
+export const PROMO_FILM_DATA: PromoFilm = {
   title: 'The Grand Budapest Hotel',
   genre: 'Drama',
   date: '2014',
