9b5c502d3682e3861eb52a394d125b600b497e74
ZuevaDarya <zuevadasha55@gmail.com>
2023-11-01 14:55:03 +0300

    добавлен тип для пропсов App

--- src/types/types.tsx
+++ src/types/types.tsx
@@ -1,10 +1,18 @@
 export type FilmCardProps = {
+  id: string | number;
   filmTitle: string;
   img: string;
+}
+
+export type PromoFilm = {
+  title: string;
+  genre: string;
+  date: string | number;
 }
 
 export type MainPageProps = {
   title: string;
   genre: string;
   date: string | number;
+  filmsInfo: FilmCardProps[];
 };
