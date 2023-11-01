5d20dc24711fd2eb9e546607f0483d28f2cdb702
ZuevaDarya <zuevadasha55@gmail.com>
2023-11-01 13:40:58 +0300

    обновление импортов

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
-import { PROMO_FILM_DATA } from './mock/film-data';
+import { PROMO_FILM_DATA } from './mocks/film-data';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
