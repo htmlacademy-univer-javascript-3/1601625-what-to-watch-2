9a448663a84ad562cd0c7ef9b2fee88c526bda7e
ZuevaDarya <zuevadasha55@gmail.com>
2023-11-02 20:57:03 +0300

    импорт моков

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -10,7 +10,7 @@ import Player from '../pages/player/player';
 import NotFound from '../pages/not-found/not-found';
 import PrivateRoute from '../private-route/private-route';
 import Layout from '../layout/layout';
-import { MY_LIST_FILMS } from '../../mocks/films';
+import { MY_LIST_FILMS, VIDEO_LINK, FILM_ADD_REVIEW } from '../../mocks/films';
 
 function App({title, genre, date, filmsInfo}: MainPageProps) {
 
@@ -33,8 +33,8 @@ function App({title, genre, date, filmsInfo}: MainPageProps) {
           }
         />
         <Route path={AppRoutes.Film} element={ <FilmPage /> }/>
-        <Route path={AppRoutes.AddReview} element={ <AddReview /> }/>
-        <Route path={AppRoutes.Player} element={ <Player /> }/>
+        <Route path={AppRoutes.AddReview} element={ <AddReview filmInfo={FILM_ADD_REVIEW} /> }/>
+        <Route path={AppRoutes.Player} element={ <Player videoLink={VIDEO_LINK} /> }/>
         <Route path='*' element={ <NotFound /> }/>
       </Routes>
 
