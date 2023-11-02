a4d1f896710de4dd56e20f053e855b9015f5db74
ZuevaDarya <zuevadasha55@gmail.com>
2023-11-02 14:50:08 +0300

    разбиение на компоненты

--- src/components/pages/film-page/film-page.tsx
+++ src/components/pages/film-page/film-page.tsx
@@ -1,60 +1,31 @@
 import { Link } from 'react-router-dom';
 import { AppRoutes } from '../../../consts';
+import Header from '../../header/header';
+import FilmCardButtonPlay from '../../film-card-button-play/film-card-button-play';
+import FilmCardButtonMylist from '../../film-card-button-mylist/film-card-button-mylist';
+import FilmCardDesc from '../../film-card-desc/film-card-desc';
+import Footer from '../../footer/footer';
 
 function FilmPage(){
   return (
     <>
       <section className="film-card film-card--full">
         <div className="film-card__hero">
-          <div className="film-card__bg">
-            <img src="img/bg-the-grand-budapest-hotel.jpg" alt="The Grand Budapest Hotel" />
-          </div>
 
-          <h1 className="visually-hidden">WTW</h1>
-
-          <header className="page-header film-card__head">
-            <div className="logo">
-              <Link to={AppRoutes.Main} className="logo__link">
-                <span className="logo__letter logo__letter--1">W</span>
-                <span className="logo__letter logo__letter--2">T</span>
-                <span className="logo__letter logo__letter--3">W</span>
-              </Link>
-            </div>
-
-            <ul className="user-block">
-              <li className="user-block__item">
-                <div className="user-block__avatar">
-                  <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
-                </div>
-              </li>
-              <li className="user-block__item">
-                <a className="user-block__link">Sign out</a>
-              </li>
-            </ul>
-          </header>
+          <Header linkLogo={AppRoutes.Main} />
 
           <div className="film-card__wrap">
             <div className="film-card__desc">
-              <h2 className="film-card__title">The Grand Budapest Hotel</h2>
-              <p className="film-card__meta">
-                <span className="film-card__genre">Drama</span>
-                <span className="film-card__year">2014</span>
-              </p>
+              {/* Передать данные выбранного на главной странице фильма */}
+              <FilmCardDesc
+                title='The Grand Budapest Hotel'
+                genre='Drama'
+                date='2014'
+              />
 
               <div className="film-card__buttons">
-                <button className="btn btn--play film-card__button" type="button">
-                  <svg viewBox="0 0 19 19" width="19" height="19">
-                    <use xlinkHref="#play-s"></use>
-                  </svg>
-                  <span>Play</span>
-                </button>
-                <button className="btn btn--list film-card__button" type="button">
-                  <svg viewBox="0 0 19 20" width="19" height="20">
-                    <use xlinkHref="#add"></use>
-                  </svg>
-                  <span>My list</span>
-                  <span className="film-card__count">9</span>
-                </button>
+                <FilmCardButtonPlay />
+                <FilmCardButtonMylist />
                 <Link to={AppRoutes.AddReview} className="btn film-card__button">Add review</Link>
               </div>
             </div>
@@ -147,19 +118,7 @@ function FilmPage(){
           </div>
         </section>
 
-        <footer className="page-footer">
-          <div className="logo">
-            <Link to={AppRoutes.Main} className="logo__link logo__link--light">
-              <span className="logo__letter logo__letter--1">W</span>
-              <span className="logo__letter logo__letter--2">T</span>
-              <span className="logo__letter logo__letter--3">W</span>
-            </Link>
-          </div>
-
-          <div className="copyright">
-            <p>© 2019 What to watch Ltd.</p>
-          </div>
-        </footer>
+        <Footer linkLogo={AppRoutes.Main} />
       </div>
     </>
   );
