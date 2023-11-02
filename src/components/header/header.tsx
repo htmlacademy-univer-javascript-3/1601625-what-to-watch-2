5f4ee99e6698a5067f9d64466781b4959402c7e4
ZuevaDarya <zuevadasha55@gmail.com>
2023-11-02 19:34:38 +0300

    разбиение на компоненты

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,4 +1,5 @@
-import { Link } from 'react-router-dom';
+import HeaderLogo from '../header-logo/header-logo';
+import HeaderUserBlock from '../header-user-block/header-user-block';
 import { PropsHeader } from '../../types/types';
 
 function Header({linkLogo}:PropsHeader){
@@ -7,28 +8,11 @@ function Header({linkLogo}:PropsHeader){
       <div className="film-card__bg">
         <img src="img/bg-the-grand-budapest-hotel.jpg" alt="The Grand Budapest Hotel" />
       </div>
-
       <h1 className="visually-hidden">WTW</h1>
 
       <header className="page-header film-card__head">
-        <div className="logo">
-          <Link to={linkLogo} className="logo__link">
-            <span className="logo__letter logo__letter--1">W</span>
-            <span className="logo__letter logo__letter--2">T</span>
-            <span className="logo__letter logo__letter--3">W</span>
-          </Link>
-        </div>
-
-        <ul className="user-block">
-          <li className="user-block__item">
-            <div className="user-block__avatar">
-              <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
-            </div>
-          </li>
-          <li className="user-block__item">
-            <a className="user-block__link">Sign out</a>
-          </li>
-        </ul>
+        <HeaderLogo linkLogo={linkLogo} />
+        <HeaderUserBlock />
       </header>
     </>
   );
--- src/components/pages/my-list/my-list.tsx
+++ src/components/pages/my-list/my-list.tsx
@@ -1,5 +1,7 @@
-import { Link } from 'react-router-dom';
+import HeaderLogo from '../../header-logo/header-logo';
+import HeaderUserBlock from '../../header-user-block/header-user-block';
 import FilmsList from '../../films-list';
+import Footer from '../../footer/footer';
 import { AppRoutes } from '../../../consts';
 import { PropsList, FilmCardProps } from '../../../types/types';
 
@@ -7,25 +9,9 @@ function MyList({list}: PropsList<FilmCardProps>){
   return (
     <div className="user-page">
       <header className="page-header user-page__head">
-        <div className="logo">
-          <Link to={AppRoutes.Main} className="logo__link">
-            <span className="logo__letter logo__letter--1">W</span>
-            <span className="logo__letter logo__letter--2">T</span>
-            <span className="logo__letter logo__letter--3">W</span>
-          </Link>
-        </div>
-
+        <HeaderLogo linkLogo={AppRoutes.Main} />
         <h1 className="page-title user-page__title">My list <span className="user-page__film-count">9</span></h1>
-        <ul className="user-block">
-          <li className="user-block__item">
-            <div className="user-block__avatar">
-              <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
-            </div>
-          </li>
-          <li className="user-block__item">
-            <a className="user-block__link">Sign out</a>
-          </li>
-        </ul>
+        <HeaderUserBlock />
       </header>
 
       <section className="catalog">
@@ -36,19 +22,7 @@ function MyList({list}: PropsList<FilmCardProps>){
         </div>
       </section>
 
-      <footer className="page-footer">
-        <div className="logo">
-          <a href="main.html" className="logo__link logo__link--light">
-            <span className="logo__letter logo__letter--1">W</span>
-            <span className="logo__letter logo__letter--2">T</span>
-            <span className="logo__letter logo__letter--3">W</span>
-          </a>
-        </div>
-
-        <div className="copyright">
-          <p>© 2019 What to watch Ltd.</p>
-        </div>
-      </footer>
+      <Footer linkLogo={AppRoutes.Main} />
     </div>
   );
 }
