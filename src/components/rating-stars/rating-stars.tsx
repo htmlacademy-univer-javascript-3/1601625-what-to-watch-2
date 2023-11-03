d63b1934624f579cc18aac2f984f42ed13e8d823
ZuevaDarya <zuevadasha55@gmail.com>
2023-11-03 09:48:55 +0300

    настроены id для рейтинга

--- src/components/rating-stars/rating-stars.tsx
+++ src/components/rating-stars/rating-stars.tsx
@@ -1,13 +1,13 @@
 import RatingStar from '../rating-star/rating-star';
 
 function RatingStars() {
-  const ratingStarsArr = Array.from({length: 10}, (item, idx) => (length - idx));
+  const ratingStarsArr = Array.from({length: 10}, (_, idx) => (10 - idx));
 
   return (
     <div className="rating__stars">
       {
-        ratingStarsArr.map((item, idx) => (
-          <RatingStar key={`star-${String(idx)}`} id={String(idx)} />
+        ratingStarsArr.map((i) => (
+          <RatingStar key={`star-${String(i)}`} id={String(i)} />
         ))
       }
     </div>
