fec6a7f4d8d9a27a0edefb4068743a7a697c5292
ZuevaDarya <zuevadasha55@gmail.com>
2023-11-11 13:48:02 +0300

    разбиение хедера на компоненты

---
+++ src/components/film-card-bg/film-card-bg.tsx
@@ -0,0 +1,11 @@
+import { FilmCardBgProps } from '../../types/types';
+
+function FilmCardBg({img, filmTitle}:FilmCardBgProps){
+  return (
+    <div className="film-card__bg">
+      <img src={`img\\${img}`} alt={filmTitle} />
+    </div>
+  );
+}
+
+export default FilmCardBg;
