1cd3e2579d604966fe86489215d2c69586829870
ZuevaDarya <zuevadasha55@gmail.com>
2023-11-12 16:03:05 +0300

    создан новый компонент

--- src/components/video-player/video-player.tsx
+++ src/components/video-player/video-player.tsx
@@ -1,6 +1,27 @@
-function VideoPlayer(){
+import { useEffect, useRef } from 'react';
+import { VideoPlayerProps } from '../../types/types';
+
+function VideoPlayer({active, src, img, filmTitle, videoTimeout, width, height}:VideoPlayerProps){
+  const videoPlayerRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    if (active) {
+      const timerId = setTimeout(()=> {
+        videoPlayerRef.current?.play();
+      }, videoTimeout);
+      return () => clearTimeout(timerId);
+    } else {
+      if(videoPlayerRef.current !== null){
+        videoPlayerRef.current.pause();
+        videoPlayerRef.current.currentTime = 0;
+      }
+    }
+  });
+
   return (
-    <video src=""></video>
+    active
+      ? <video ref={videoPlayerRef} src={src} width={width} height={height} muted loop />
+      : <img src={`img/${img}`} alt={filmTitle} width={width} height={height}/>
   );
 }
 
