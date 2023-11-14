b73255c81e0dd977887b1fa601f73d56269766ab
ZuevaDarya <zuevadasha55@gmail.com>
2023-11-14 14:02:56 +0300

    добавлены моковые данные

--- src/mocks/reviews.ts
+++ src/mocks/reviews.ts
@@ -1,16 +1,16 @@
-import { FilmReviewsProps } from '../types/types';
+import { FilmReviews } from '../types/types';
 
-export const REVIEWS: FilmReviewsProps[] = [
+export const REVIEWS: FilmReviews[] = [
   {
     id: '1DtaWAfwl',
-    text: 'Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director&apos;s funniest and most exquisitely designed films in years.',
+    text: 'Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director\'s funniest and most exquisitely designed films in years.',
     author: 'Kate Muir',
     date: 'December 24, 2016',
     rating: '8,9',
   },
   {
     id: '2Afarpfs',
-    text: 'Anderson&apos;s films are too precious for some, but for those of us willing to lose ourselves in them, they&apos;re a delight. &quot;The Grand Budapest Hotel&quot; is no different, except that he has added a hint of gravitas to the mix, improving the recipe.',
+    text: 'Anderson\'s films are too precious for some, but for those of us willing to lose ourselves in them, they\'re a delight. "The Grand Budapest Hotel" is no different, except that he has added a hint of gravitas to the mix, improving the recipe.',
     author: 'Bill Goodykoontz',
     date: 'November 18, 2015',
     rating: '8,0',
@@ -24,7 +24,7 @@ export const REVIEWS: FilmReviewsProps[] = [
   },
   {
     id: '4IdfiaawIc',
-    text: 'I didn&apos;t find it amusing, and while I can appreciate the creativity, it&apos;s an hour and 40 minutes I wish I could take back.',
+    text: 'I didn\'t find it amusing, and while I can appreciate the creativity, it\'s an hour and 40 minutes I wish I could take back.',
     author: 'Amanda Greever',
     date: 'November 18, 2015',
     rating: '8,0',
