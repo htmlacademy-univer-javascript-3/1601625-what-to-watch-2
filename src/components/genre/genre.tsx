e266ecd940af89b724538a9fcfab3e387a8f806a
ZuevaDarya <zuevadasha55@gmail.com>
2023-11-28 16:07:29 +0300

    создан новый компонент для жанров

---
+++ src/components/genre/genre.tsx
@@ -0,0 +1,15 @@
+import { GenreProps } from '../../types/types';
+import './genre.css';
+
+function Genre({ genre, activeClass, setActiveGenre }: GenreProps) {
+  return (
+    <li
+      className={`catalog__genres-item genre-item_hover ${activeClass}`}
+      onClick={() => setActiveGenre(genre)}
+    >
+      <a className="catalog__genres-link">{genre}</a>
+    </li>
+  );
+}
+
+export default Genre;
