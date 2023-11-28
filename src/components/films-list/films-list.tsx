845866967b9a99f50387ae0ebbb2eae5207d7ec5
ZuevaDarya <zuevadasha55@gmail.com>
2023-11-28 16:06:51 +0300

    вывод фильмов в соответствие жанру

--- src/components/films-list/films-list.tsx
+++ src/components/films-list/films-list.tsx
@@ -2,25 +2,42 @@ import { useState } from 'react';
 import FilmCard from '../film-card/film-card';
 import {PropsList, FilmCardProps} from '../../types/types';
 import { VIDEO_LINK } from '../../mocks/films';
+import { useAppSelector } from '../../hooks';
+import { GenresEnum } from '../../consts';
 
 function FilmsList({list}: PropsList<FilmCardProps>){
   const [activeCardId, setActiveCardId] = useState<string | null>(null);
 
+  const activeGenre = useAppSelector((state) => state.filterGenres.genre);
+
   return (
-    <>
-      {list.map(({id, filmTitle, img}) => (
+    activeGenre === GenresEnum.AllGenres
+      ? list.map(({id, filmTitle, img, genre}) => (
         <FilmCard
           key={id}
           id={id}
           filmTitle={filmTitle}
           img={img}
           active={activeCardId === id}
+          genre={genre}
 
           setActiveCardId={setActiveCardId}
           videoLink={VIDEO_LINK}
         />
-      ))}
-    </>
+      ))
+      : list.filter(({genre}) => genre === activeGenre).map(({id, filmTitle, img, genre}) => (
+        <FilmCard
+          key={id}
+          id={id}
+          filmTitle={filmTitle}
+          img={img}
+          active={activeCardId === id}
+          genre={genre}
+
+          setActiveCardId={setActiveCardId}
+          videoLink={VIDEO_LINK}
+        />
+      ))
   );
 }
 
