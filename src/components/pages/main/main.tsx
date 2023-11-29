cf1ffceb3e865275c4039558f486517776a76990
ZuevaDarya <zuevadasha55@gmail.com>
2023-11-29 13:18:13 +0300

    добавлена логика работы фильтров

--- src/components/pages/main/main.tsx
+++ src/components/pages/main/main.tsx
@@ -1,27 +1,53 @@
+import { useMemo, useState } from 'react';
 import FilmCardBg from '../../film-card-bg/film-card-bg';
 import Header from '../../header/header';
 import FilmCardPoster from '../../film-card-poster/film-card-poster';
 import FilmCardButtonPlay from '../../film-card-button-play/film-card-button-play';
 import FilmCardButtonMylist from '../../film-card-button-mylist/film-card-button-mylist';
 import FilmCardDesc from '../../film-card-desc/film-card-desc';
-import GenresEnum from '../../genres-list/genres-list';
+import GenresList from '../../genres-list/genres-list';
 import FilmsList from '../../films-list/films-list';
-import CatalogButton from '../../catalog-button/catalog-button';
+import ShowMoreButton from '../../show-more-button/show-more-button';
 import Footer from '../../footer/footer';
-import { MainPageProps } from '../../../types/types';
+import { MainPageProps, GetFilmsByGenreFunc } from '../../../types/types';
+import { GenresEnum, MAX_NUM_FILMS } from '../../../consts';
+import { useAppSelector } from '../../../hooks';
 
-function MainPage({title, genre, year, poster, bgImg, filmsInfo}: MainPageProps){
+function MainPage({ title, genre, year, poster, bgImg }: MainPageProps) {
+  const [maxNumFilms, setMaxNumFilms] = useState(MAX_NUM_FILMS);
+
+  const activeGenre = useAppSelector((state) => state.filterGenres.genre);
+  const filmsInfo = useAppSelector((state) => state.filterGenres.filmCardList);
+
+  const getFilmsByGenre: GetFilmsByGenreFunc = (list) => {
+    if (activeGenre === GenresEnum.AllGenres) {
+      return list;
+    } else {
+      return list.filter((film) => film.genre === activeGenre);
+    }
+  };
+
+  const handlerShowMoreClick = () => {
+    setMaxNumFilms((max) => max + MAX_NUM_FILMS);
+  };
+
+  const filmsByGenre = getFilmsByGenre(filmsInfo);
+
+  const shownFilms = useMemo(() => (
+    filmsByGenre.filter((_, idx) => idx <= maxNumFilms)
+  ), [filmsByGenre, maxNumFilms]);
+
   return (
     <>
       <section className="film-card">
         <FilmCardBg img={bgImg} filmTitle={title} />
         <h1 className="visually-hidden">WTW</h1>
 
-        <Header linkLogo="/" classes='film-card__head' />
+        <Header linkLogo="/" classes="film-card__head" />
 
         <div className="film-card__wrap">
           <div className="film-card__info">
-            <FilmCardPoster imgSrc={poster} imgTitle={title}/>
+            <FilmCardPoster imgSrc={poster} imgTitle={title} />
 
             <div className="film-card__desc">
               <FilmCardDesc title={title} genre={genre} year={year} />
@@ -39,12 +65,16 @@ function MainPage({title, genre, year, poster, bgImg, filmsInfo}: MainPageProps)
         <section className="catalog">
           <h2 className="catalog__title visually-hidden">Catalog</h2>
 
-          <GenresEnum />
+          <GenresList />
 
           <div className="catalog__films-list">
-            <FilmsList list={filmsInfo}/>
+            <FilmsList list={shownFilms} />
           </div>
-          <CatalogButton />
+          {
+            shownFilms.length >= maxNumFilms
+              ? <ShowMoreButton onShowMoreClick={handlerShowMoreClick} />
+              : null
+          }
         </section>
 
         <Footer linkLogo="/" />
