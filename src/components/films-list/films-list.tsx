074d44ebf5ddff5126ee063a660fbd5b513a628d
ZuevaDarya <zuevadasha55@gmail.com>
2023-11-29 13:17:15 +0300

    вынесение логики фильтров в main компонент

--- src/components/films-list/films-list.tsx
+++ src/components/films-list/films-list.tsx
@@ -1,20 +1,15 @@
 import { useState } from 'react';
 import FilmCard from '../film-card/film-card';
-import {PropsList, FilmCardProps} from '../../types/types';
+import { PropsList, FilmCardProps } from '../../types/types';
 import { VIDEO_LINK } from '../../mocks/films';
-import { useAppSelector } from '../../hooks';
-import { GenresEnum } from '../../consts';
 
-function FilmsList({list}: PropsList<FilmCardProps>){
+function FilmsList({ list }: PropsList<FilmCardProps>) {
   const [activeCardId, setActiveCardId] = useState<string | null>(null);
 
-  const activeGenre = useAppSelector((state) => state.filterGenres.genre);
-
   return (
-    activeGenre === GenresEnum.AllGenres
-      ?
-      <>
-        { list.map(({id, filmTitle, img, genre}) => (
+    <>
+      {
+        list.map(({ id, filmTitle, img, genre }) => (
           <FilmCard
             key={id}
             id={id}
@@ -22,28 +17,12 @@ function FilmsList({list}: PropsList<FilmCardProps>){
             img={img}
             active={activeCardId === id}
             genre={genre}
-
             setActiveCardId={setActiveCardId}
             videoLink={VIDEO_LINK}
           />
-        ))}
-      </>
-      :
-      <>
-        { list.filter(({genre}) => genre === activeGenre).map(({id, filmTitle, img, genre}) => (
-          <FilmCard
-            key={id}
-            id={id}
-            filmTitle={filmTitle}
-            img={img}
-            active={activeCardId === id}
-            genre={genre}
-
-            setActiveCardId={setActiveCardId}
-            videoLink={VIDEO_LINK}
-          />
-        ))}
-      </>
+        ))
+      }
+    </>
   );
 }
 
