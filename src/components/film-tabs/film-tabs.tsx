69c9e7dbc43edcf453cdca28d074965ce3a42f88
ZuevaDarya <zuevadasha55@gmail.com>
2023-11-29 13:16:06 +0300

    изменение имени функции

--- src/components/film-tabs/film-tabs.tsx
+++ src/components/film-tabs/film-tabs.tsx
@@ -2,18 +2,21 @@ import { useState } from 'react';
 import FilmTabsNav from '../film-tabs-nav/film-tabs-nav';
 import FilmTabsContainer from '../film-tabs-container/film-tabs-container';
 import { TABS } from '../../consts';
-import { AddActiveClassFunction, FilmTabsProps } from '../../types/types';
+import { AddActiveClassFunc, FilmTabsProps } from '../../types/types';
 
-function FilmTabs({overviewInfo, detailsInfo, reviewsInfo}:FilmTabsProps){
+function FilmTabs({ overviewInfo, detailsInfo, reviewsInfo }: FilmTabsProps) {
   const [activeTab, setActiveTab] = useState<number>(0);
 
-  const addActiveClass:AddActiveClassFunction = (idx, activeClass) => (
-    activeTab === idx ? activeClass : ''
-  );
+  const addActiveClass: AddActiveClassFunc = (idx, activeClass) =>
+    activeTab === idx ? activeClass : '';
 
-  return(
+  return (
     <>
-      <FilmTabsNav tabs={TABS} setActiveTab={setActiveTab} addActiveClass={addActiveClass}/>
+      <FilmTabsNav
+        tabs={TABS}
+        setActiveTab={setActiveTab}
+        addActiveClass={addActiveClass}
+      />
       <FilmTabsContainer
         activeTab={activeTab}
         overviewInfo={overviewInfo}
--- src/utils/getRatingDescription.ts
+++ src/utils/getRatingDescription.ts
@@ -1,7 +1,7 @@
 import { RatingDescription } from '../consts';
-import { GetRatingDescriptionFunction } from '../types/types';
+import { GetRatingDescriptionFunc } from '../types/types';
 
-export const getRatingDescription: GetRatingDescriptionFunction = (rating:number) => {
+export const getRatingDescription: GetRatingDescriptionFunc = (rating:number) => {
   if (rating >= 0 && rating < 3){
     return RatingDescription.Bad;
   } else if (rating >= 3 && rating < 5){
