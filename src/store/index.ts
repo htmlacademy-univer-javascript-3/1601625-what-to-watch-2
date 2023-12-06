1106c969be5610df6ad2edd119719e4f2177e23c
ZuevaDarya <zuevadasha55@gmail.com>
2023-12-06 13:43:25 +0300

    подключение thunk redux

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { filterGenresReducer } from './reducer';
+import { filterGenresReducer} from './reducer';
+import { createAPI } from '../services/api';
+
+export const api = createAPI();
 
 export const store = configureStore({
   reducer: {
     filterGenres: filterGenresReducer,
-  }
+    // authorisation: authorisationReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: api,
+      },
+    }),
 });
