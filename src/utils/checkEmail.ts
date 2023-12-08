7df61f26dff80eb8aa03847886c3c4f93b912973
ZuevaDarya <zuevadasha55@gmail.com>
2023-12-08 19:59:23 +0300

    добавлена функция для проверки логина

---
+++ src/utils/checkEmail.ts
@@ -0,0 +1,11 @@
+import { checkEmailFunc } from '../types/types';
+
+export const checkEmail: checkEmailFunc = (email) => {
+  const regexp = new RegExp(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+
+  if (email.match(regexp)){
+    return true;
+  } else {
+    return false;
+  }
+};
