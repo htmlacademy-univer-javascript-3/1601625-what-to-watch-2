949be010876910a10ebdb0710a6ff53f878533df
ZuevaDarya <zuevadasha55@gmail.com>
2023-12-26 20:06:02 +0300

    рефакторинг кода

--- src/utils/changeDateFormat/changeDateFormat.test.ts
+++ src/utils/changeDateFormat/changeDateFormat.test.ts
@@ -2,6 +2,6 @@ import { changeDateFormat } from './changeDateFormat';
 
 describe('Function: changeDateFormat', () => {
   it('should return data in format: Full month day, year', () => {
-    expect(changeDateFormat('2023-12-03T21:00:00.315Z')).toEqual('December 3, 2023');
+    expect(changeDateFormat('2023-12-03T21:00:00')).toEqual('December 3, 2023');
   });
 });
